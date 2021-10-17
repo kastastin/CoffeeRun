6804fa6b93bb8b2ed7fa816f9594d6e3d609aed3
kastastin <kostya242421@gmai.com>
2021-10-17 15:20:19 +0300

    part_5

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
     var CheckList = App.CheckList;
     var myTruck = new Truck('1', new DataStore());
     window.myTruck = myTruck;
@@ -17,5 +18,7 @@
         myTruck.createOrder.call(myTruck, data);
         checkList.addRow.call(checkList, data);
     });
-    console.log(formHandler);
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+
 })(window);
